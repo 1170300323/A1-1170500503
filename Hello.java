9f6059d696355adc7b29ca0366953d5e37d6255d
LXT <1623916874@qq.com>
2019-07-03 18:52:22 +0800

    first commit

---
+++ Hello.java
@@ -0,0 +1,18 @@
+package hello;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import lab6.message;
+import lab6.other;
+import lab6.reached;
+
+public class Hello {	
+		
+	public static void main(String[] args) throws InterruptedException {
+	  System.out.println("Hello world");
+	}
+}
