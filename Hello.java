090bb8e6d4e392822a5cefea9e3a6bc166559979
Fyunaru <34539909+1172100327@users.noreply.github.com>
2019-07-07 22:37:08 +0800

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -6,13 +6,11 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import lab6.message;
-import lab6.other;
-import lab6.reached;
 
 public class Hello {	
 		
 	public static void main(String[] args) throws InterruptedException {
 	  System.out.println("Hello world");
+	  System.out.println("1172100327");
 	}
 }
